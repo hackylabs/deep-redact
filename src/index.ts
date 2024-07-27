6141e8fca7be26c611c72ed192ae8f23aca1fdb2
Ben Green <ben.green@greencodemedia.co.uk>
2024-07-27 09:04:58 +0100

    FEAT: gracefully handle unsupported values

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,4 @@
-export type Types = 'string' | 'number' | 'bigint' | 'boolean' | 'object'
+export type Types = 'string' | 'number' | 'bigint' | 'boolean' | 'object' | 'function'
 
 export interface BlacklistKeyConfig {
   fuzzyKeyMatch?: boolean
@@ -18,11 +18,15 @@ export interface RedactionConfig {
   replacement?: string
   remove?: boolean
   types?: Types[]
+  serialise?: boolean,
+  unsupportedTransformer?: (value: unknown) => unknown
 }
 
 const normaliseString = (key: string): string => key.toLowerCase().replace(/\W/g, '')
 
 export class Redaction {
+  private circularReference: WeakSet<object> | null = new WeakSet()
+
   private readonly config: Required<RedactionConfig> = {
     blacklistedKeys: [],
     stringTests: [],
@@ -33,6 +37,8 @@ export class Redaction {
     replaceStringByLength: false,
     replacement: '[REDACTED]',
     types: ['string'],
+    serialise: true,
+    unsupportedTransformer: Redaction.unsupportedTransformer,
   }
 
   constructor(config: RedactionConfig) {
@@ -53,6 +59,59 @@ export class Redaction {
     }
   }
 
+  private static unsupportedTransformer = (value: unknown): unknown => {
+    if (typeof value === 'bigint') {
+      return {
+        __unsupported: {
+          type: 'bigint',
+          value: value.toString(),
+          radix: 10,
+        },
+      }
+    }
+
+    if (value instanceof Error) {
+      return {
+        __unsupported: {
+          type: 'error',
+          name: value.name,
+          message: value.message,
+          stack: value.stack,
+        },
+      }
+    }
+
+    if (value instanceof RegExp) {
+      return {
+        __unsupported: {
+          type: 'regexp',
+          source: value.source,
+          flags: value.flags,
+        },
+      }
+    }
+
+    if (value instanceof Date) return value.toISOString()
+
+    return value
+  }
+
+  private removeCircular = (value: unknown): unknown => {
+    if (!(value instanceof Object)) return value
+    if (!this.circularReference?.has(value)) {
+      this.circularReference?.add(value)
+      return value
+    }
+
+    return '__circular__'
+  }
+
+  private redactString = (value: string, parentShouldRedact = false): string | undefined => {
+    if (!this.config.stringTests.some((test) => test.test(value)) && !parentShouldRedact) return value
+    if (this.config.replaceStringByLength) return this.config.replacement.repeat(value.length)
+    return this.config.remove ? undefined : this.config.replacement
+  }
+
   private static complexShouldRedact = (key: string, config: BlacklistKeyConfig): boolean => {
     if (config.fuzzyKeyMatch && config.caseSensitiveKeyMatch) return key.includes(config.key)
 
@@ -70,26 +129,16 @@ export class Redaction {
   }
 
   private deepRedact = (value: unknown, parentShouldRedact = false): unknown => {
-    if (typeof value === 'function' || typeof value === 'symbol' || typeof value === 'undefined' || value === null) return value
+    if (value === undefined || value === null) return value
 
-    if (!(value instanceof Object)) {
-      // @ts-expect-error - we already know that value is not a function, symbol, undefined, null, or an object
-      if (!this.config.types.includes(typeof value)) return value
+    let safeValue = this.removeCircular(value)
+    safeValue = this.config.unsupportedTransformer(safeValue)
+    if (!(safeValue instanceof Object)) {
+      // @ts-expect-error - we already know that safeValue is not a function, symbol, undefined, null, or an object
+      if (!this.config.types.includes(typeof safeValue)) return safeValue
+      if (typeof safeValue === 'string') return this.redactString(safeValue, parentShouldRedact)
 
-      let shouldRedact = parentShouldRedact
-
-      if (typeof value === 'string') {
-        shouldRedact = shouldRedact || this.config.stringTests.some((test) => test.test(value))
-
-        if (!shouldRedact) return value
-        if (this.config.remove) return undefined
-
-        return this.config.replaceStringByLength
-          ? this.config.replacement.repeat(value.length)
-          : this.config.replacement
-      }
-
-      if (!shouldRedact) return value
+      if (!parentShouldRedact) return safeValue
 
       return this.config.remove
         ? undefined
@@ -100,13 +149,18 @@ export class Redaction {
       return this.config.remove ? undefined : this.config.replacement
     }
 
-    if (Array.isArray(value)) return value.map((val) => this.deepRedact(val, parentShouldRedact))
+    if (Array.isArray(safeValue)) return safeValue.map((val) => this.deepRedact(val, parentShouldRedact))
 
-    return Object.fromEntries(Object.entries(value).map(([key, val]) => {
-      const shouldRedact = parentShouldRedact || this.shouldReactObjectValue(key)
+    return Object.fromEntries(Object.entries(safeValue).map(([key, val]) => {
+      const shouldRedact = parentShouldRedact || this.shouldRedactObjectValue(key)
       return [key, this.deepRedact(val, shouldRedact)]
     }))
   }
 
-  redact = (value: unknown): unknown => this.deepRedact(value)
+  redact = (value: unknown): unknown => {
+    this.circularReference = new WeakSet()
+    const redacted = this.deepRedact(value)
+    this.circularReference = null
+    return this.config.serialise ? JSON.stringify(redacted) : redacted
+  }
 }
