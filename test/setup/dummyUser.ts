2e42e63883ba9348ee65d297304aa6039d81a878
Ben Green <ben.green@greencodemedia.co.uk>
2024-10-05 15:09:24 +0100

    tests(bench): set XML redaction baseline speed

--- test/setup/dummyUser.ts
+++ test/setup/dummyUser.ts
@@ -114,4 +114,73 @@ const extendedDummyUser = {
 
 extendedDummyUser.someCircular.obj = extendedDummyUser
 
-export { dummyUser, extendedDummyUser }
+const dummyUserXml = `<dummyUser><id>1</id>
+<firstName>Emily</firstName>
+<lastName>Johnson</lastName>
+<maidenName>Smith</maidenName>
+<age>28</age>
+<gender>female</gender>
+<email>emily.johnson@x.dummyjson.com</email>
+<phone>+81 965-431-3024</phone>
+<username>emilys</username>
+<password>emilyspass</password>
+<birthDate>1996-5-30</birthDate>
+<image>https://dummyjson.com/icon/emilys/128</image>
+<bloodGroup>O-</bloodGroup>
+<height>193.24</height>
+<weight>63.16</weight>
+<eyeColor>Green</eyeColor>
+<hair>
+    <color>Brown</color>
+    <type>Curly</type>
+</hair>
+<ip>42.48.100.32</ip>
+<crypto>
+    <coin>Bitcoin</coin>
+    <wallet>0xb9fc2fe63b2a6c003f1c324c3bfa53259162181a</wallet>
+    <network>Ethereum (ERC20)</network>
+</crypto>
+<address>
+    <street>626 Main Street</street>
+    <city>Phoenix</city>
+    <state>Mississippi</state>
+    <stateCode>MS</stateCode>
+    <postalCode>29112</postalCode>
+    <coordinates>
+        <lat>-77.16213</lat>
+        <lng>-92.084824</lng>
+    </coordinates>
+    <country>United States</country>
+</address>
+<macAddress>47:fa:41:18:ec:eb</macAddress>
+<university>University of Wisconsin--Madison</university>
+<bank>
+    <cardExpire>03/26</cardExpire>
+    <cardNumber>9289760655481815</cardNumber>
+    <cardType>Elo</cardType>
+    <currency>CNY</currency>
+    <iban>YPUXISOBI7TTHPK2BR3HAIXL</iban>
+</bank>
+<company>
+    <department>Engineering</department>
+    <name>Dooley, Kozey and Cronin</name>
+    <title>Sales Manager</title>
+    <address>
+        <address>263 Tenth Street</address>
+        <city>San Francisco</city>
+        <state>Wisconsin</state>
+        <stateCode>WI</stateCode>
+        <postalCode>37657</postalCode>
+        <coordinates>
+            <lat>71.814525</lat>
+            <lng>-161.150263</lng>
+        </coordinates>
+        <country>United States</country>
+    </address>
+</company>
+<ein>977-175</ein>
+<ssn>900-590-289</ssn>
+<userAgent>Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/96.0.4664.93 Safari/537.36</userAgent>
+<role>admin</role></dummyUser>`
+
+export { dummyUser, extendedDummyUser, dummyUserXml }
