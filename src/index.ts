59e78bbaba1b0d0adad00b9d81c6ff6e6ae699d4
Ben Green <ben.green@holidayextras.com>
2025-01-30 16:53:11 +0000

    fix(JS doc): missing throws statement

--- src/index.ts
+++ src/index.ts
@@ -160,6 +160,7 @@ class DeepRedact {
    * Redact the provided value. The value will be stripped of any circular references and other unsupported data types, before being redacted according to the configuration and finally serialised if required.
    * @param {unknown} value The value to redact.
    * @returns {unknown} The redacted value.
+   * @throws {Error} If the value cannot be serialised.
    */
   redact = (value: unknown): unknown => {
     return this.maybeSerialise(this.redactorUtils.recurse(this.rewriteUnsupported(value)))
