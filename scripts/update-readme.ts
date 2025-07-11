d253d80818b64d7a3096057279c02394ffd6473d
Ben Green <ben.green@holidayextras.com>
2025-07-11 14:19:50 +0100

    chore(bench): remove bench test results

--- scripts/update-readme.ts
+++ scripts/update-readme.ts
@@ -5,16 +5,12 @@ import { join } from 'node:path'
 import { mainOptions } from './mainOptions'
 import { blacklistKeyConfig } from './blacklistKeyConfig'
 import { stringTestConfig } from './stringTestConfig'
-import { benchmarks } from './benchTable'
-import { updateBenchChart } from './benchChart'
 
-updateBenchChart()
 const readme = readFileSync(join(__dirname, 'templates', 'README.txt')).toString()
 
 const output = readme
   .replace('<--MAIN_OPTIONS-->', mainOptions)
   .replace('<--BLACKLIST_KEY_CONFIG-->', blacklistKeyConfig)
   .replace('<--STRING_TEST_CONFIG-->', stringTestConfig)
-  .replace('<--BENCH-->', benchmarks)
 
 writeFileSync(join(__dirname, '..', 'README.md'), output)
