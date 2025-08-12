71a91e63a8e05a7d61320157b392aa3a7c021c7c
Ben Green <ben.green@holidayextras.com>
2025-08-12 12:43:06 +0100

    test: manual redaction test

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,7 @@
 import type { DeepRedactConfig, RedactorUtilsConfig, Types, Transformer, ComplexStringTest, ObjectPathConfig, BaseDeepRedactConfig, OrganisedTransformers, TransformerConfig } from './types'
 import { organisedStandardTransformers, standardTransformers } from './utils/standardTransformers'
 import RedactorUtils from './utils'
+import { deepRedactPaths, stringPattern, xmlPattern } from '../test/setup/blacklist'
 
 class DeepRedact {
   /**
@@ -55,4 +56,53 @@ export {
   type TransformerConfig,
   standardTransformers,
   organisedStandardTransformers,
+}
+
+if (require.main === module) {
+  const deepRedact = new DeepRedact({
+    paths: deepRedactPaths,
+    stringTests: [{
+      pattern: xmlPattern,
+      replacer: (value, pattern) => value.replace(pattern, '<$1>[REDACTED]</$1>'),
+    }],
+  })
+
+  const now = Date.now()
+  console.log(deepRedact.redact({
+    name: 'John Doe',
+    email: 'john.doe@example.com',
+    phone: '1234567890',
+    password: 'password123',
+    birthDate: '1990-01-01',
+    ip: '127.0.0.1',
+    ein: '123456789',
+    ssn: '123456789',
+    address: {
+      addressLine1: '123 Main St',
+      addressLine2: 'Apt 1',
+      city: 'Anytown',
+      state: 'CA',
+      postalCode: '12345',
+      country: 'USA',
+    },
+    company: {
+      name: 'Acme Inc',
+      address: {
+        addressLine1: '123 Main St',
+        addressLine2: 'Apt 1',
+        city: 'Anytown',
+        state: 'CA',
+        postalCode: '12345',
+        country: 'USA',
+      },
+      phone: '1234567890',
+      email: 'john.doe@example.com',
+      password: 'password123',
+      birthDate: '1990-01-01',
+      ip: '127.0.0.1',
+      ein: '123456789',
+      ssn: '123456789',
+    },
+  }))
+  console.log(`Time taken: ${Date.now() - now}ms`)
 }
